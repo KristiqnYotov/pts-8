0b4c71174bd0308ec2303bdd63174df11311411f
Krisko Yotov <yotov1525@gmail.com>
2022-03-29 08:46:46 +0300

    Add project files.

---
+++ pts8mo uprj/Program.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pts8mo_uprj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebDriver driver = new ChromeDriver("C:\\Users\\Krisko Yotov\\source\\repos\\pts8mo uprj\\pts8mo uprj\\bin\\Debug");
+            driver.Url = "http://demo.guru99.com/test/login.html";
+            WebElement email = (WebElement)driver.FindElement(By.Id("email"));
+            WebElement password = (WebElement)driver.FindElement(By.Id("passwd"));
+           
+            email.SendKeys("abcd@gmail.com");
+            password.SendKeys("abcdefghlkjl");
+
+            WebElement login = (WebElement)driver.FindElement(By.Id("SubmitLogin"));
+
+
+            email.SendKeys("abcd@gmail.com");
+            password.SendKeys("abcdefghlkjl");
+            login.Click();
+
+        }
+    }
+}
